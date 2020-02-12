48ee8294ab58bb5d7d2e0d56202105003cd94324
Marcus Deuss <marcus.deuss@me.com>
2020-02-12 12:30:05 +0100

    Initial Commit

---
+++ swiftAG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftAG
+//
+//  Created by Marcus Deuß on 12.02.20.
+//  Copyright © 2020 Marcus Deuß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
