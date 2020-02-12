cb7338a4e0ddc2f1ddde43e4b8c31339cf546221
Marcus Deuss <marcus.deuss@me.com>
2020-02-12 12:40:38 +0100

    initiales Commit

---
+++ swiftAG/BankKonto.swift
@@ -0,0 +1,79 @@
+//
+//  BankKonto.swift
+//  swiftAG
+//
+//  Created by Marcus Deuß on 12.02.20.
+//  Copyright © 2020 Marcus Deuß. All rights reserved.
+//
+
+import Foundation
+
+
+// Ein Bankkonto soll folgende Möglichkeiten bieten:
+// Gewünschte Fähigkeiten:
+// 1. Konto eröffnen
+// 2. Kontostand anzeigen lassen
+// 3. Geld einzahlen auf Konto
+// 4. Geld abheben vom Konto
+// 5. Geld vom eigenen Konto auf ein anderes Konto überweisen
+
+class BankKonto{
+    // Eigenschaften
+    
+    // die neue Kontonummer, muss eindeutig sein, zwei Personen dürfen nie die selbe Nummer haben
+    private(set) var kontoNummer: Int = 0
+    
+    // der Eigentümer Name des Kontos
+    var besitzer: String
+    
+    // der aktuelle Kontostand
+    private(set) var kontoStand: Float = 0.0
+    
+    // initiale Bankkonto Nummer
+    private static var seed = 1000
+    
+    // Initialisierung
+    
+    init(besitzer: String){
+        kontoNummer = BankKonto.seed    // Kontonummer wird automatisch erzeugt
+        BankKonto.seed += 1             // danach +1, damit neues Konto anders lautet
+        self.besitzer = besitzer        // Besitzer zuweisen
+        kontoStand = 0.0                // und initialer Kontostand = 0.0
+    }
+    
+    // direkt einen Startbetrag einzahlen beim Anlegen des Objektes
+    init(besitzer: String, einzahlung: Float){
+        kontoNummer = BankKonto.seed    // Kontonummer wird automatisch erzeugt
+        BankKonto.seed += 1             // danach +1, damit neues Konto anders lautet
+        self.besitzer = besitzer        // Besitzer zuweisen
+        kontoStand = einzahlung         // und initialer Kontostand = 0.0
+    }
+    
+    // Methoden
+    
+    func macheAbbuchung(betrag: Float){
+        if(kontoStand - betrag >= 0.0){
+            kontoStand -= betrag
+        }
+        else{
+            print("so viel Geld ist nicht mehr auf dem Konto!")
+        }
+    }
+    
+    func macheEinzahlung(betrag: Float){
+        kontoStand += betrag
+    }
+    
+    // aktuellen Kontostand anzeigen
+    func zeigeKontostand(){
+        print("Der Kontostand von \(besitzer) lautet: \(String(kontoStand))€")
+    }
+    
+    func überweisen(betrag: Float, konto: BankKonto){
+        // Geld vom eigenen Konto abbuchen
+        macheAbbuchung(betrag: betrag)
+        // und auf das andere Konto überweisen
+        konto.macheEinzahlung(betrag: betrag)
+    }
+}
+
--- swiftAG/ViewController.swift
+++ swiftAG/ViewController.swift
@@ -13,8 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        testeBankkonto()
     }
 
 
+    func testeBankkonto(){
+        // Testen der Konto Klasse
+
+        let konto1 = BankKonto(besitzer: "Marcus")
+
+        let konto2 = BankKonto(besitzer: "Sandra")
+
+        print(konto1.besitzer)
+        print(konto1.kontoNummer)
+
+
+        print(konto2.kontoNummer)
+        konto1.macheEinzahlung(betrag: 200.0)
+        konto1.macheAbbuchung(betrag: 100.0)
+        konto1.macheAbbuchung(betrag: 13.20)
+
+
+        konto2.macheEinzahlung(betrag: 500.0)
+
+        print(konto1.kontoStand)
+        print(konto2.kontoStand)
+
+        konto1.überweisen(betrag: 45.0, konto: konto2)
+        print(konto1.kontoStand)
+        print(konto2.kontoStand)
+    }
 }
 
